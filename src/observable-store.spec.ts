0327f8db27e9afff33371d9da5172139ad7f173e
ElAndyG <andrewgagne@outlook.com>
2019-07-04 18:38:12 -0400

    added additional tests for state history

--- src/observable-store.spec.ts
+++ src/observable-store.spec.ts
@@ -2,12 +2,12 @@ import { skip } from 'rxjs/operators';
 
 import { ObservableStore } from './observable-store';
 
-describe('Observable Store', () => {
-  interface FakeState {
-    prop1: string;
-    prop2: string;
-  }
+interface FakeState {
+  prop1: string;
+  prop2: string;
+}
 
+describe('Observable Store', () => {
   class FakeStore extends ObservableStore<FakeState> {
     updateProp1(value: string) {
       this.setState({ prop1: value });
@@ -45,5 +45,41 @@ describe('Observable Store', () => {
 
     expect(receiveUpdate).toBeTruthy();
     sub.unsubscribe();
+  });
+
+  describe('State History', () => {
+    const Update_Prop1 = 'Update_Prop1';
+    class UserStore extends ObservableStore<FakeState> {
+      updateProp1(value: string) {
+        this.setState({ prop1: value }, Update_Prop1);
+      }
+      get currentState() {
+        return this.getState();
+      }
+    }
+
+    it('should have no history on initialize', () => {
+      const fakeStore = new UserStore({ trackStateHistory: true });
+
+      console.log('init test: ', fakeStore.stateHistory);
+
+      expect(fakeStore.stateHistory.length).toEqual(0);
+    });
+
+    it('should add one record to state history', () => {
+      const fakeStore = new UserStore({ trackStateHistory: true });
+
+      fakeStore.updateProp1('test');
+      expect(fakeStore.stateHistory.length).toEqual(1);
+    });
+
+    it('should add 2 records to state history', () => {
+      const fakeStore = new UserStore({ trackStateHistory: true });
+      expect(fakeStore.stateHistory.length).toEqual(0);
+
+      fakeStore.updateProp1('test');
+      fakeStore.updateProp1('test');
+      expect(fakeStore.stateHistory.length).toEqual(2);
+    });
   });
 });
