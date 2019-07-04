f62990533b2d8667704051e55f478c42205c5f32
ElAndyG <andrewgagne@outlook.com>
2019-07-04 15:19:22 -0400

    add a placeholder spec file

---
+++ src/observable-store.spec.ts
@@ -0,0 +1,10 @@
+import { ObservableStore } from './observable-store';
+
+describe('Observable Store', () => {
+
+  it('should be a placeholder and run', () => {
+    const initialNumber = 1;
+
+    expect(initialNumber).toBe(1);
+  });
+});
