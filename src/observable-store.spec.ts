bef1e110254ffd3aef4444903dca41938eee573b
ElAndyG <andrewgagne@outlook.com>
2019-07-04 15:58:28 -0400

    tests set state will change a property

--- src/observable-store.spec.ts
+++ src/observable-store.spec.ts
@@ -1,10 +1,25 @@
 import { ObservableStore } from './observable-store';
 
 describe('Observable Store', () => {
+  interface FakeState {
+    prop1: string;
+    prop2: string;
+  }
 
-  it('should be a placeholder and run', () => {
-    const initialNumber = 1;
+  class FakeStore extends ObservableStore<FakeState> {
+    updateProp1(value: string) {
+      this.setState({ prop1: value });
+    }
+    get currentState() {
+      return this.getState();
+    }
+  }
 
-    expect(initialNumber).toBe(1);
+  it('should set a single property', () => {
+    const store = new FakeStore({});
+
+    store.updateProp1('test');
+
+    expect(store.currentState.prop1).toEqual('test');
   });
 });
