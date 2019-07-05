c0dc7dd5de376302a9317d805d5f0e5edf312161
ElAndyG <andrewgagne@outlook.com>
2019-07-04 22:25:05 -0400

    adjust tests for static store

--- src/observable-store.spec.ts
+++ src/observable-store.spec.ts
@@ -2,114 +2,62 @@ import { skip } from 'rxjs/operators';
 
 import { ObservableStore } from './observable-store';
 
-interface FakeState {
+const Update_Prop1 = 'Update_Prop1';
+
+interface MockState {
   prop1: string;
   prop2: string;
 }
-
-describe('Observable Store', () => {
-  class FakeStore extends ObservableStore<FakeState> {
-    updateProp1(value: string) {
-      this.setState({ prop1: value });
-    }
-    get currentState() {
-      return this.getState();
-    }
+class MockStore extends ObservableStore<MockState> {
+  updateProp1(value: string) {
+    this.setState({ prop1: value }, Update_Prop1);
   }
 
+  updateForTestAction(value: string, action: string) {
+    this.setState({ prop1: value }, action);
+  }
+
+  get currentState() {
+    return this.getState();
+  }
+}
+describe('Observable Store', () => {
+  let mockStore = new MockStore({ trackStateHistory: true });
+
   it('should set a single property', () => {
-    const store = new FakeStore({});
+    mockStore.updateProp1('test');
 
-    store.updateProp1('test');
-
-    expect(store.currentState.prop1).toEqual('test');
+    expect(mockStore.currentState.prop1).toEqual('test');
   });
 
-  // we will skip 1 to account for the initial BehaviorSubject<T> value
-  it('should NOT receive notification if no state has changed', () => {
-    const store = new FakeStore({});
-    let receiveUpdate = false;
-    const sub = store.stateChanged.pipe(skip(1)).subscribe(() => (receiveUpdate = true));
+  describe('Subscriptions', () => {
+    // we will skip 1 to account for the initial BehaviorSubject<T> value
+    it('should NOT receive notification if no state has changed', () => {
+      let receiveUpdate = false;
+      const sub = mockStore.stateChanged.pipe(skip(1)).subscribe(() => (receiveUpdate = true));
 
-    expect(receiveUpdate).toBeFalsy();
-    sub.unsubscribe();
-  });
-
-  // we will skip 1 to account for the initial BehaviorSubject<T> value
-  it('should receive notification when state has been changed', () => {
-    const store = new FakeStore({});
-    let receiveUpdate = false;
-    const sub = store.stateChanged.pipe(skip(1)).subscribe(() => (receiveUpdate = true));
-
-    store.updateProp1('test');
-
-    expect(receiveUpdate).toBeTruthy();
-    sub.unsubscribe();
-  });
-
-  describe('State History', () => {
-    const Update_Prop1 = 'Update_Prop1';
-    class UserStore extends ObservableStore<FakeState> {
-      updateProp1(value: string) {
-        this.setState({ prop1: value }, Update_Prop1);
-      }
-      get currentState() {
-        return this.getState();
-      }
-    }
-
-    it('should have no history on initialize', () => {
-      const fakeStore = new UserStore({ trackStateHistory: true });
-
-      console.log('init test: ', fakeStore.stateHistory);
-
-      expect(fakeStore.stateHistory.length).toEqual(0);
+      expect(receiveUpdate).toBeFalsy();
+      sub.unsubscribe();
     });
 
-    it('should add one record to state history', () => {
-      const fakeStore = new UserStore({ trackStateHistory: true });
+    // we will skip 1 to account for the initial BehaviorSubject<T> value
+    it('should receive notification when state has been changed', () => {
+      let receiveUpdate = false;
+      const sub = mockStore.stateChanged.pipe(skip(1)).subscribe(() => (receiveUpdate = true));
 
-      fakeStore.updateProp1('test');
-      expect(fakeStore.stateHistory.length).toEqual(1);
-    });
+      mockStore.updateProp1('test');
 
-    it('should add 2 records to state history', () => {
-      const fakeStore = new UserStore({ trackStateHistory: true });
-      expect(fakeStore.stateHistory.length).toEqual(0);
-
-      fakeStore.updateProp1('test');
-      fakeStore.updateProp1('test');
-      expect(fakeStore.stateHistory.length).toEqual(2);
+      expect(receiveUpdate).toBeTruthy();
+      sub.unsubscribe();
     });
   });
 
-  describe('Can I have 2 stores?', () => {
-    const Update_Prop1A = 'Update_Prop1-A';
-    const Update_Prop1B = 'Update_Prop1-B';
+  describe('Action', () => {
+    it('should add valid action to stateHistory', () => {
+      const mockAction = 'Mock_Action';
+      mockStore.updateForTestAction('test', mockAction);
 
-    class MockStoreA extends ObservableStore<FakeState> {
-      updateProp1(value: string) {
-        this.setState({ prop1: value }, Update_Prop1A);
-      }
-      get currentState() {
-        return this.getState();
-      }
-    }
-
-    class MockStoreB extends ObservableStore<FakeState> {
-      updateProp1(value: string) {
-        this.setState({ prop1: value }, Update_Prop1B);
-      }
-      get currentState() {
-        return this.getState();
-      }
-    }
-    it('should have different histories', () => {
-      const mockStoreA = new MockStoreA({ trackStateHistory: true });
-      const mockStoreB = new MockStoreB({ trackStateHistory: true });
-
-      mockStoreA.updateProp1('test');
-      expect(mockStoreB.stateHistory.length).toBe(0);
+      expect(mockStore.stateHistory[mockStore.stateHistory.length - 1].action).toEqual(mockAction);
     });
   });
 });
