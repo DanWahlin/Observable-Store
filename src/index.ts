d60b765cdc6fffaaaff4ac898553b62bab1181ba
Dan Wahlin <danwahlin@iMac-3.local>
2019-10-24 17:43:24 -0700

    Removed export of clonerservice

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { ObservableStore } from './observable-store';
 export { ObservableStoreSettings, ObservableStoreGlobalSettings, 
         StateWithPropertyChanges, StateHistory, StateSliceSelector } from './interfaces';
-// export { ClonerService } from './utilities/cloner.service';
