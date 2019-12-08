a64889820bf16264ba66718eff824563d404eda7
Dan Wahlin <danwahlin@iMac-3.local>
2019-12-08 01:53:48 -0700

    Removed old code comments

--- modules/observable-store-extensions/angular/angular-devtools-extension.ts
+++ modules/observable-store-extensions/angular/angular-devtools-extension.ts
@@ -5,21 +5,13 @@ export class AngularDevToolsExtension {
     private rootElements: any;
     private router: any;
     private ngZone: any;
-    // private providers: any;
-    // private location: any;
-    // private routeTriggered = false;
 
     constructor() {
         this.rootElements = this.window.ng.probe(this.window.getAllAngularRootElements()[0]);
         const providers = this.rootElements.injector.view.root.ngModule._providers;        
         this.router = providers.find(p => p && p.constructor && p.constructor.name === 'Router');
-        // this.location = this.providers.find(p => p && p.constructor && p.constructor.name === 'Location');
 
         this.ngZone = this.getNgZone();
-
-        // if (this.router) {
-        //     this.hookRouter();
-        // }
     }
 
     navigate(path: string) {
@@ -46,36 +38,5 @@ export class AngularDevToolsExtension {
             console.log(e);
         }
     }
-
-    // hookRouter() {
-    //     try {
-    //         const currentPath = this.location.path();
-    //         this.devToolsService.setState({ [this.routerPropertyName]: { path: currentPath } }, `ROUTE_NAVIGATION [${currentPath}]`);
-
-    //         this.router.events
-    //             .pipe(
-    //                 filter(event => {
-    //                     const navEnd = event.toString().startsWith('NavigationEnd');
-    //                     // See if router.navigateByUrl() was triggered by the extension
-    //                     // if so then don't add the route into the state
-    //                     if (navEnd && this.routeTriggered) {
-    //                         this.routeTriggered = false;
-    //                         return false;
-    //                     }
-    //                     return navEnd;
-    //                 }),
-    //                 map(event => {
-    //                     return { path: this.location.path() };
-    //                 }),
-    //                 distinctUntilChanged()
-    //             ).subscribe(router => {
-    //                 this.devToolsService.setState({ [this.routerPropertyName]: router }, `ROUTE_NAVIGATION [${router.path}]`);
-    //             });
-
-    //     }
-    //     catch (e) {
-    //         console.log(e);
-    //     }
-    // }
 
 }
