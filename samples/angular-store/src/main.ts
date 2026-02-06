af8aec648eb7db7808345503621cfa5cf898946e
Dan Wahlin <dwahlin@xmlforasp.net>
2026-02-06 07:26:30 +0000

    Re-enable Redux DevTools extension in angular-store sample

--- samples/angular-store/src/main.ts
+++ samples/angular-store/src/main.ts
@@ -2,10 +2,17 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { ObservableStore } from '@codewithdan/observable-store';
+import { ReduxDevToolsExtension } from '@codewithdan/observable-store-extensions';
 
 // Set ObservableStore globalSettings here since
 // it'll be called before the rest of the app loads
-ObservableStore.globalSettings = { isProduction: false };
+ObservableStore.globalSettings = {
+  isProduction: false,
+  trackStateHistory: true,
+};
+
+// Enable Redux DevTools integration
+ObservableStore.addExtension(new ReduxDevToolsExtension());
 
 bootstrapApplication(AppComponent, appConfig)
   .catch(err => console.error(err));
